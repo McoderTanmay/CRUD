2679fefae1420783afa758d8cb60ec62e70d58b8
Tanmay <tanmaysontakke99@gmail.com>
2024-04-27 11:29:03 +0530

    loading animation added

--- client/crud/src/components/fetch/FetchRecords.jsx
+++ client/crud/src/components/fetch/FetchRecords.jsx
@@ -1,34 +1,50 @@
-import { useContext, useState} from "react";
+import { useContext, useState } from "react";
 import Records from "./Record";
 import { fetchContext } from "../../contex/FetchContex";
 import "./fetchRecords.css";
+import { ring } from "ldrs";
 
-function Fetch({onRecordId, onUpdateRecordId}) {
+ring.register();
+
+function Fetch({ onRecordId, onUpdateRecordId }) {
   const { records, isLoading, error } = useContext(fetchContext);
-  function idHandeler(id){
+  function idHandeler(id) {
     onRecordId(id);
   }
-  function updateIdHandeler(id){
+  function updateIdHandeler(id) {
     onUpdateRecordId(id);
   }
 
   return (
     <>
       <div className="outer">
+      
         {error ? (
           <p>An error encountered while fetching</p>
         ) : isLoading ? (
-          <p>Loading...</p>
+          <div className="loading">
+            <l-ring
+            size="40"
+            stroke="5"
+            bg-opacity="0"
+            speed="2"
+            color="black"
+          ></l-ring>
+          </div>
         ) : (
-          records && records.map((record) => (
-            <Records key={record?._id} details={record} onDelete={idHandeler} onUpdate={updateIdHandeler}></Records>
+          records &&
+          records.map((record) => (
+            <Records
+              key={record?._id}
+              details={record}
+              onDelete={idHandeler}
+              onUpdate={updateIdHandeler}
+            ></Records>
           ))
         )}
       </div>
     </>
-    
   );
 }
-
 
 export default Fetch;
--- client/crud/src/utils/services.js
+++ client/crud/src/utils/services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://crud-git-master-tanmay-sontakkes-projects.vercel.app";
 
 export const getRequest = async (url) => {
   try {
